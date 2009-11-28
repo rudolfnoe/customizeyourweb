f66f421854efb176acc422de1a57801f66f44fa3
rudolf.noe <rudolf.noe@01d9ef64-1fbd-11de-b56b-c9554f25afe1>
2009-11-28 21:16:00 +0000

    Bugfix to 0.4.1

---
+++ branches/0.4.1.1/src/chrome/content/cyw/editing/common/targetdefinition/TargetDefinitionXblValidator.js
@@ -0,0 +1,48 @@
+with(customizeyourweb){
+(function(){
+   /*
+    * @param binding targetdefinition targetDefinitionBinding: bindingfield
+    * @param DOMWindow targetWin: targetwin
+    * @param boolean allowMultiTargetDef: allow the target definition to match multiple targets
+    */
+   function TargetDefinitionXblValidator(targetDefinitionBinding, targetWin, allowMultiTargetDef){
+      this.AbstractValidator()
+      this.targetDefinitionBinding = targetDefinitionBinding
+      this.targetWin = targetWin
+      //Default is false
+      this.allowMultiTargetDef = arguments.length>=2?allowMultiTargetDef:false
+      this.registerListener()
+   }
+   
+   TargetDefinitionXblValidator.prototype = {
+      constructor: TargetDefinitionXblValidator,
+      TargetDefinitionXblValidator: TargetDefinitionXblValidator,
+      
+      isValid: function(){
+         var targetDef = this.targetDefinitionBinding.getTargetDefinition()
+         if(targetDef==null){
+            return false
+         }
+         //Target is not in page and it is not optional
+         if (!targetDef.isTargetInPage(this.targetWin) && !targetDef.isTargetOptional()){
+            return false
+         }
+         var targets = targetDef.getTargets(this.targetWin)
+         if(targets.length > 1 && !this.allowMultiTargetDef){
+            return false
+         }
+         return true
+      },
+      
+      registerListener: function(){
+         this.targetDefinitionBinding.addValueChangedListener(Utils.bind(function(event){
+            this.setValidState(this.isValid())
+         }, this))
+      }
+   }
+   
+   ObjectUtils.extend(TargetDefinitionXblValidator, "AbstractValidator", customizeyourweb)
+   
+   Namespace.bindToNamespace("customizeyourweb", "TargetDefinitionXblValidator", TargetDefinitionXblValidator)
+})()
+}
