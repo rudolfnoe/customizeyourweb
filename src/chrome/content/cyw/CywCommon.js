c3b258cceafe99e6fe9f157eb01774086fc0cb61
rudolf.noe <rudolf.noe@01d9ef64-1fbd-11de-b56b-c9554f25afe1>
2011-02-12 14:24:31 +0000

    CYW 0.4.3.2Build201102101800 AMO-Version

--- src/chrome/content/cyw/CywCommon.js
+++ src/chrome/content/cyw/CywCommon.js
@@ -5,7 +5,7 @@
    var CywCommon = {
       
 		//Constants
-      CYW_VERSION: "0.4.3.1Build201012161000",
+      CYW_VERSION: "0.4.3.2Build201102101800",
       
 		CHROME_CONTENT_URL: "chrome://customizeyourweb/content/",
 		CYW_CHROME_URL: "chrome://customizeyourweb/content/cyw/",
